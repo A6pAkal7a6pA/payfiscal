152759f96d32a1fec28edfec6c75bd8e82680c93
Max Kuleshov <mxm.klshv@gmail.com>
2020-12-08 16:36:28 +0200

    some changes

--- js/script.js
+++ js/script.js
@@ -8,17 +8,40 @@ $(document).ready(function () {
     $('.popup-enter').fadeIn();
     $('body').addClass('lock');
   });
+  $('#popup-reg').click(function() {
+    $('.popup-reg').fadeIn();
+    $('.popup-enter').fadeOut();
+    $('body').addClass('lock');
+  });
+  $('#popup-enter').click(function() {
+    $('.popup-reg').fadeOut();
+    $('.popup-enter').fadeIn();
+    $('body').addClass('lock');
+  });
   $('.popup-enter__exit').click(function() {
-    // $('.popup-enter').addClass('active');
+    $('.popup-reg').fadeOut();
     $('.popup-enter').fadeOut();
     $('body').removeClass('lock');
   });
 
 });
-$(document).mouseup(function (e) {
-  var popup = $('.popup-enter__body');
-  if (e.target!=popup[0] && popup.has(e.target).length === 0) {
-    $('.popup-enter').fadeOut();
-    $('body').removeClass('lock');
-  }
-});
+// $(document).mouseup(function (e) {
+//   var popup = $('.popup-enter__body', '.popup-reg__body');
+//   if (e.target!=popup && popup.has(e.target).length === 0) {
+//     $('.popup-enter').fadeOut();
+//     $('.popup-reg').fadeOut();
+//     $('body').removeClass('lock');
+//   }
+// });
+const anchors = document.querySelectorAll('a[href*="#"');
+
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const blockID = anchor.getAttribute('href');
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: "smooth", 
+      block: 'start'
+    })
+  });
+}
