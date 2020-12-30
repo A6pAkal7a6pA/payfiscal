cd6a35da4a63441a11b145518743a757cfa43caf
Max Kuleshov <mxm.klshv@gmail.com>
2020-12-30 23:21:37 +0200

    some changes

--- js/script-pa.js
+++ js/script-pa.js
@@ -25,45 +25,58 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.js-pa-add-dep').click(function () {
     $('.pa-add-dep').fadeIn();
-    // $('body').addClass('lock');
+    $('body').addClass('lock');
   });
   $('.pa-add-dep__exit').click(function () {
     $('.pa-add-dep').fadeOut();
-    // $('body').removeClass('lock');
+    $('body').removeClass('lock');
   });
 
+});
+
+$(document).keydown(function(event) { 
+  if (event.keyCode == 27) { 
+    $('.pa-add-dep').fadeOut();
+    $('.pa-add-edit').fadeOut();
+    $('.pa-list-dep').fadeOut();
+    $('.pa-list-user').fadeOut();
+    $('.pa-add-user').fadeOut();
+    $('.pa-oper').fadeOut();
+    $('.pa-list-ppo').fadeOut();
+    $('body').removeClass('lock');
+  }
 });
 
 $(document).ready(function () {
   $('.js-pa-add-user').click(function () {
     $('.pa-add-user').fadeIn();
-    // $('body').addClass('lock');
+    $('body').addClass('lock');
   });
   $('.pa-add-user__exit').click(function () {
     $('.pa-add-user').fadeOut();
-    // $('body').removeClass('lock');
+    $('body').removeClass('lock');
   });
 });
 
 $(document).ready(function () {
   $('.js-pa-add-edit').click(function () {
     $('.pa-add-edit').fadeIn();
-    // $('body').addClass('lock');
+    $('body').addClass('lock');
   });
   $('.pa-add-edit__exit').click(function () {
     $('.pa-add-edit').fadeOut();
-    // $('body').removeClass('lock');
+    $('body').removeClass('lock');
   });
 });
 
 $(document).ready(function () {
   $('.js-pa-list-dep').click(function () {
     $('.pa-list-dep').fadeIn();
-    // $('body').addClass('lock');
+    $('body').addClass('lock');
   });
   $('.pa-list-dep__exit').click(function () {
     $('.pa-list-dep').fadeOut();
-    // $('body').removeClass('lock');
+    $('body').removeClass('lock');
   });
 });
 
@@ -83,8 +96,6 @@ $(document).ready(function () {
     "language": {
       "info": "Показано<br> _END_ позицій з _MAX_"
     },
-    "columnDefs": [
-    ]
   });
 });
 
@@ -97,21 +108,75 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.js-pa-list-user').click(function () {
     $('.pa-list-user').fadeIn();
-    // $('body').addClass('lock');
+    $('body').addClass('lock');
   });
   $('.pa-list-user__exit').click(function () {
     $('.pa-list-user').fadeOut();
-    // $('body').removeClass('lock');
+    $('body').removeClass('lock');
   });
 });
 
 $(document).ready(function () {
   $('.js-pa-oper').click(function () {
     $('.pa-oper').fadeIn();
-    // $('body').addClass('lock');
+    $('body').addClass('lock');
   });
   $('.pa-oper__exit').click(function () {
     $('.pa-oper').fadeOut();
-    // $('body').removeClass('lock');
+    $('body').removeClass('lock');
   });
 });
+
+$(document).ready(function () {
+  $('.pa-list-ppo__table').DataTable({
+    ordering: true,
+    lengthMenu: [5, 4, 3, 2, 1],
+    paging: true,
+    "pagingType": "numbers",
+    "language": {
+      "info": "Показано<br> _END_ позицій з _MAX_",
+      "lengthMenu": "Показати _MENU_"
+    },
+  });
+});
+
+$(document).ready(function () {
+  $('.js-pa-list-ppo').click(function () {
+    $('.pa-list-ppo').fadeIn();
+    $('body').addClass('lock');
+  });
+  $('.pa-list-ppo__exit').click(function () {
+    $('.pa-list-ppo').fadeOut();
+    $('body').removeClass('lock');
+  });
+});
+
+let select = function () {
+  let selectHeader = document.querySelectorAll('.pa-add-user-select__header');
+  let selectItem = document.querySelectorAll('.pa-add-user-select__item');
+
+  selectHeader.forEach(item => {
+      item.addEventListener('click', selectToggle)
+  });
+
+  selectItem.forEach(item => {
+      item.addEventListener('click', selectChoose)
+  });
+
+  function selectToggle() {
+      this.parentElement.classList.toggle('is-active');
+  }
+
+  function selectChoose() {
+      let text = this.innerText,
+          select = this.closest('.pa-add-user-select'),
+          currentText = select.querySelector('.pa-add-user-select__current');
+      currentText.innerText = text;
+      select.classList.remove('is-active');
+
+  }
+
+};
+
+
+select();
